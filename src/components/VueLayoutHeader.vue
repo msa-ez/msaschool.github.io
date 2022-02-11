00b6aa59310ecc613a0f20f07c0a2076b80f4de1
kibum0405 <kibum0405@uengine.org>
2022-02-11 15:07:52 +0900

    수정

--- src/components/VueLayoutHeader.vue
+++ src/components/VueLayoutHeader.vue
@@ -31,7 +31,7 @@
                 </div>
 
                 <div class="flex flex-col items-center px-3   mr-auto  sm:flex-row"
-                     style="justify-content:flex-end;margin-right:5%;">
+                     style="justify-content:flex-end;margin-right:1.5%;">
                     <!-- <div class="modal-container">
                         <input id="modal-toggle" type="checkbox">
                         <label class="modal-btn"
