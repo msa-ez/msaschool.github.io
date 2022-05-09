974a3bc1fc5ef1e345fce1a6d587c8c8004f7407
kibum0405 <kibum0405@uengine.org>
2022-05-09 14:26:17 +0900

    내용 추가

---
+++ src/content/msaschool/operation/biz/collaborativeEventstorming.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <mark-down class="content">
+### 협업을 통한 이벤트스토밍
+
+- Food Delivery 프로세스 모델을 참조하여 Design Level 및 추가 시나리오까지 반영된 이벤트스토밍 모델을 협업하여 완성해 본다.
+
+#### 사전준비
+- 4~5인의 팀으로 Teaming하고 각 Team별 팀장을 지정한다.
+- 교육 운영자는 각 팀을 위한 소회의실을 사전 구성하고, 교육생별 소회의실을 매핑한다.
+
+#### 협업환경 구성
+- 팀별 이벤트스토밍 모델을 공유하고 협업하는 방법을 학습한다.
+> 각 팀별 팀장님들과 함께 How-to를 익힌다.
+- 팀장님들은 팀을 위한 모델을 새로이 생성하고, 프로젝트 이름을 지정한다.
+> 네이밍 : food delivery 1, food delivery n
+- 모델을 저장하고, 메뉴의 'share'를 눌러 'share everyone'으로 공유설정한다.
+- 팀원들은 MSA Ez 'Public' 탭에서 우리팀의 모델에 들어가 'Join' 신청한다.
+> 이 때, 모델의 Owner인 팀장님은 팀원들의 Join 요청을 수락함으로써 티밍환경을 구성한다.
+
+#### 협업 이벤트스토밍 
+-  Food Delivery (배민예제)를 바탕으로 Bounded Context별 팀원이 매핑되어 이벤트스토밍한다.
+-  Bounded Context간의 컨텍스트 매핑을 적용하며 인터페이스를 적용한다.
+-  시나리오에 없는 배달의 민족 기능(Rider 픽업, etc)도 추가해 본다.
+- 완료된 ES 모델이 기능/비기능 요건을 만족하는지 팀내에서 검증해 본다.
+        </mark-down>
+    </div>
+</template>
+
+
+<script>
+    // @group 06_02_05
+    export default {
+        name:'collaborativeEventstorming',
+        data() {
+            return {}
+        },
+        props: {
+            "[이벤트스토밍] Collaborative Eventstorming": {
+                type: String
+            },
+        },
+    }
+</script>
