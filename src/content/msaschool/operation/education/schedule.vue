1f15b3621a7fc871b05f3e2345cdbc66b735d804
kibum0405 <kibum0405@uengine.org>
2022-05-23 14:46:32 +0900

    교과정 안내 하나로 통일

--- src/content/msaschool/operation/education/schedule.vue
+++ src/content/msaschool/operation/education/schedule.vue
@@ -1,7 +1,7 @@
 <template>
     <div style="margin-top:80px;">
         <h2 style = "margin:0 0 10px 0;">MSA School 교과정 안내</h2>
-        <div class="basics-box" style="display:inline-flex; width:100%; height:550px;">
+        <div class="basics-box clearfix">
             <div class="card-box">
                 <div class="card-title">Quick</div>
                 <div class="card-subtitle">Quick Understanding CNA</div>
@@ -121,293 +121,8 @@
                     마이크로서비스의 전 과정을 심도있게 학습할 수 있는 과정으로 분석/설계, 구현, 배포, 운영 및 캡스톤 프로젝트를 포함하는 인텐시브 Course
                 </div>
                 <div style="padding-left:5px;  height:60px; line-height:40px; display:flex;">
-                    <div style="font-size:18px; font-weight:900;">247.5만원 ~ 420만원</div>
+                    <div class="enterprise-cost-text">247.5만원 ~ 420만원</div>
                     <div style="margin:3px 0 0 3px; font-size:14px;">/1인 14일</div>
-                </div>
-                <div class="card-second-contents">
-                    • 14 days<br>
-                    • 교육일정 협의<br>
-                    • 비대면 화상교육 (Zoom)<br>
-                    • 교육 교재, 클라우드 IDE/계정 제공<br>
-                    • CSP(AWS, Azure, GCP) 택 1<br>
-                    • 최대인원 25명<br>
-                </div>
-                <div style="text-align:center; height:90px; line-height:90px;">
-                    <v-btn class="card-details-btn"
-                        :href="`http://www.msaschool.io/operation/education/enterprise-full-day14-days-course/`"
-                        ><span style="color:white !important;">세부사항</span>
-                    </v-btn>
-                </div>
-            </div>
-        </div>
-
-
-
-        <div class="middle-box">
-            <div style="display:inline-flex;">
-                <div class="card-box">
-                    <div class="card-title">Quick</div>
-                    <div class="card-subtitle">Quick Understanding CNA</div>
-                    <div class="card-contents">
-                        마이크로서비스를 처음 접하거나 향후 도입 또는 컨설팅예정인 기업 또는 개인을 위한 MSA, 클라우드 네이티브 앱에 대한 개념 정립을 위한 Half-day Course
-                    </div>
-                    <div style="padding-left:5px; font-size:24px; font-weight:900; height:60px; line-height:40px;">Free</div>
-                    <div class="card-second-contents">
-                        • 3~4 hours<br>
-                        • 매월 2주차 수요일<br>
-                        • 비대면 화상교육<br>
-                        • 최대인원 20명<br>
-                    </div>
-                    <div style="text-align:center; height:90px; line-height:90px;">
-                        <v-btn class="card-details-btn"
-                            :href="`http://www.msaschool.io/operation/education/quick-understanding-cna/`"
-                            ><span style="color:white !important;">세부사항</span>
-                        </v-btn>
-                    </div>
-                </div>
-                
-                <div class="card-box">
-                    <div class="card-title"
-                    >Standard
-                    </div>
-                    <div class="card-subtitle">Standard CNA</div>
-                    <div class="card-contents">
-                        마이크로서비스 모델링에서 클라우드 네이티브 앱 구현까지 MSA 이론과 클라우드 환경에서의 구현,배포,운영 실습을 포함하는 full Course
-                    </div>
-                    <div style="padding-left:5px;  height:60px; line-height:40px; display:flex;">
-                        <div style="font-size:24px; font-weight:900;">160만원</div>
-                        <div style="margin:3px 0 0 3px; font-size:14px;">/1인 4일</div>
-                    </div>
-                    <div class="card-second-contents">
-                        • 4 days<br>
-                        • 매월 3주차 월~목요일<br>
-                        • 비대면 화상교육 (Zoom)<br>
-                        • Biz/Dev과정만 선택수강 가능<br>
-                        • 교재, 클라우드 IDE/계정 제공<br>
-                        • CSP(AWS, Azure, GCP) 택 1<br>
-                        • 최대인원 25명<br>
-                    </div>
-                    <div style="text-align:center; height:90px; line-height:90px;">
-                        <v-btn class="card-details-btn"
-                            :href="`https://www.msaschool.io/operation/education/standard-cna/`"
-                            ><span style="color:white !important;">세부사항</span>
-                        </v-btn>
-                    </div>
-                </div>
-                
-                <div class="card-box">
-                    <div class="card-title"
-                    >Flipped
-                    </div>
-                    <div class="card-subtitle">Flipped Learning CNA</div>
-                    <div class="card-contents">
-                        Standard CNA 과정 중 Biz Part(MSA 이론, DDD 분석/설계)는 주강사가 진행하고,  Dev 및 Ops Part는 주강사의 영상 학습 후 보조강사가 실습을 주도하는 full Course 
-                    </div>
-                    <div style="padding-left:5px;  height:60px; line-height:40px; display:flex;">
-                        <div style="font-size:24px; font-weight:900;">100만원</div>
-                        <div style="margin:3px 0 0 3px; font-size:14px;">/1인 4일</div>
-                    </div>
-                    <div class="card-second-contents">
-                        • 4 days<br>
-                        • 매월 4주차 월~목요일<br>
-                        • 비대면 화상교육 (Zoom)<br>
-                        • Biz/Dev과정만 선택수강 가능<br>
-                        • 교재, 클라우드 IDE/계정 제공<br>
-                        • CSP(AWS, Azure, GCP) 택 1<br>
-                        • 최대인원 25명<br>
-                    </div>
-                    <div style="text-align:center; height:90px; line-height:90px;">
-                        <v-btn class="card-details-btn"
-                            :href="`http://www.msaschool.io/operation/education/flipped-learning4-days-cna-course/`"
-                            ><span style="color:white !important;">세부사항</span>
-                        </v-btn>
-                    </div>
-                </div>
-            </div>
-            
-            <div style="display:inline-flex;">
-                <div class="card-box">
-                    <div class="card-title"
-                    >One Point
-                    </div>
-                    <div class="card-subtitle">One Point Lesson</div>
-                    <div class="card-contents">
-                        MSA를 적용하려는 기업이나, 컨설팅을 수행하려는 업체가 주도적으로 정한 도메인 주제를 기반으로 인터랙티브하게 진행되는 MSA 교육
-                    </div>
-                    <div style="padding-left:5px;  height:60px; line-height:40px; display:flex;">
-                        <div style="font-size:24px; font-weight:900;">50만원</div>
-                        <div style="margin:3px 0 0 3px; font-size:14px;">/ 시간</div>
-                    </div>
-                    <div class="card-second-contents">
-                        • 1시간 단위<br>
-                        • 일정 선택 가능<br>
-                        • 비대면 화상교육 (Zoom)<br>
-                        • 교재, 클라우드 IDE/계정 제공<br>
-                        • CSP(AWS, Azure, GCP) 택 1<br>
-                        • 최대인원 10명<br>
-                    </div>
-                    <div style="text-align:center; height:90px; line-height:90px;">
-                        <v-btn class="card-details-btn"
-                            :href="`http://www.msaschool.io/operation/education/one-point-lesson/`"
-                            ><span style="color:white !important;">세부사항</span>
-                        </v-btn>
-                    </div>
-                </div>
-
-                <div class="card-box">
-                    <div class="card-title"
-                    >Enterprise
-                    </div>
-                    <div class="card-subtitle">Enterprise Full-day</div>
-                    <div class="card-contents">
-                        마이크로서비스의 전 과정을 심도있게 학습할 수 있는 과정으로 분석/설계, 구현, 배포, 운영 및 캡스톤 프로젝트를 포함하는 인텐시브 Course
-                    </div>
-                    <div style="padding-left:5px;  height:60px; line-height:40px; display:flex;">
-                        <div style="font-size:18px; font-weight:900;">247.5만원 ~ 420만원</div>
-                        <div style="margin:3px 0 0 3px; font-size:14px;">/1인 14일</div>
-                    </div>
-                    <div class="card-second-contents">
-                        • 14 days<br>
-                        • 교육일정 협의<br>
-                        • 비대면 화상교육 (Zoom)<br>
-                        • 교육 교재, 클라우드 IDE/계정 제공<br>
-                        • CSP(AWS, Azure, GCP) 택 1<br>
-                        • 최대인원 25명<br>
-                    </div>
-                    <div style="text-align:center; height:90px; line-height:90px;">
-                        <v-btn class="card-details-btn"
-                            :href="`http://www.msaschool.io/operation/education/enterprise-full-day14-days-course/`"
-                            ><span style="color:white !important;">세부사항</span>
-                        </v-btn>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="mobile-box">
-            <div style="display:inline-flex">
-                <div class="card-box">
-                    <div class="card-title">Quick</div>
-                    <div class="card-subtitle">Quick Understanding CNA</div>
-                    <div class="card-contents">
-                        마이크로서비스를 처음 접하거나 향후 도입 또는 컨설팅예정인 기업 또는 개인을 위한 MSA, 클라우드 네이티브 앱에 대한 개념 정립을 위한 Half-day Course
-                    </div>
-                    <div style="padding-left:5px; font-size:24px; font-weight:900; height:60px; line-height:40px;">Free</div>
-                    <div class="card-second-contents">
-                        • 3~4 hours<br>
-                        • 매월 2주차 수요일<br>
-                        • 비대면 화상교육<br>
-                        • 최대인원 20명<br>
-                    </div>
-                    <div style="text-align:center; height:90px; line-height:90px;">
-                        <v-btn class="card-details-btn"
-                            :href="`http://www.msaschool.io/operation/education/quick-understanding-cna/`"
-                            ><span style="color:white !important;">세부사항</span>
-                        </v-btn>
-                    </div>
-                </div>
-                
-                <div class="card-box">
-                    <div class="card-title"
-                    >Standard
-                    </div>
-                    <div class="card-subtitle">Standard CNA</div>
-                    <div class="card-contents">
-                        마이크로서비스 모델링에서 클라우드 네이티브 앱 구현까지 MSA 이론과 클라우드 환경에서의 구현,배포,운영 실습을 포함하는 full Course
-                    </div>
-                    <div style="padding-left:5px;  height:60px; line-height:40px; display:flex;">
-                        <div style="font-size:24px; font-weight:900;">160만원</div>
-                        <div style="margin:3px 0 0 3px; font-size:14px;">/1인 4일</div>
-                    </div>
-                    <div class="card-second-contents">
-                        • 4 days<br>
-                        • 매월 3주차 월~목요일<br>
-                        • 비대면 화상교육 (Zoom)<br>
-                        • Biz/Dev과정만 선택수강 가능<br>
-                        • 교재, 클라우드 IDE/계정 제공<br>
-                        • CSP(AWS, Azure, GCP) 택 1<br>
-                        • 최대인원 25명<br>
-                    </div>
-                    <div style="text-align:center; height:90px; line-height:90px;">
-                        <v-btn class="card-details-btn"
-                            :href="`https://www.msaschool.io/operation/education/standard-cna/`"
-                            ><span style="color:white !important;">세부사항</span>
-                        </v-btn>
-                    </div>
-                </div>
-            </div>
-            
-            <div style="display:inline-flex">
-                <div class="card-box">
-                    <div class="card-title"
-                    >Flipped
-                    </div>
-                    <div class="card-subtitle">Flipped Learning CNA</div>
-                    <div class="card-contents">
-                        Standard CNA과정 중 Biz Part(MSA 이론, DDD 분석/설계)는 주강사가 진행하고,  Dev 및 Ops Part는 주강사의 영상 학습 후 보조강사가 실습을 주도하는 full Course 
-                    </div>
-                    <div style="padding-left:5px;  height:60px; line-height:40px; display:flex;">
-                        <div style="font-size:24px; font-weight:900;">100만원</div>
-                        <div style="margin:3px 0 0 3px; font-size:14px;">/1인 4일</div>
-                    </div>
-                    <div class="card-second-contents">
-                        • 4 days<br>
-                        • 매월 4주차 월~목요일<br>
-                        • 비대면 화상교육 (Zoom)<br>
-                        • Biz/Dev과정만 선택수강 가능<br>
-                        • 교재, 클라우드 IDE/계정 제공<br>
-                        • CSP(AWS, Azure, GCP) 택 1<br>
-                        • 최대인원 25명<br>
-                    </div>
-                    <div style="text-align:center; height:90px; line-height:90px;">
-                        <v-btn class="card-details-btn"
-                            :href="`http://www.msaschool.io/operation/education/flipped-learning4-days-cna-course/`"
-                            ><span style="color:white !important;">세부사항</span>
-                        </v-btn>
-                    </div>
-                </div>
-                
-                <div class="card-box">
-                    <div class="card-title"
-                    >One Point
-                    </div>
-                    <div class="card-subtitle">One Point Lesson</div>
-                    <div class="card-contents">
-                        MSA를 적용하려는 기업이나, 컨설팅을 수행하려는 업체가 주도적으로 정한 도메인 주제를 기반으로 인터랙티브하게 진행되는 MSA 교육
-                    </div>
-                    <div style="padding-left:5px;  height:60px; line-height:40px; display:flex;">
-                        <div style="font-size:24px; font-weight:900;">50만원</div>
-                        <div style="margin:3px 0 0 3px; font-size:14px;">/ 시간</div>
-                    </div>
-                    <div class="card-second-contents">
-                        • 1시간 단위<br>
-                        • 일정 선택 가능<br>
-                        • 비대면 화상교육 (Zoom)<br>
-                        • 교재, 클라우드 IDE/계정 제공<br>
-                        • CSP(AWS, Azure, GCP) 택 1<br>
-                        • 최대인원 10명<br>
-                    </div>
-                    <div style="text-align:center; height:90px; line-height:90px;">
-                        <v-btn class="card-details-btn"
-                            :href="`http://www.msaschool.io/operation/education/one-point-lesson/`"
-                            ><span style="color:white !important;">세부사항</span>
-                        </v-btn>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card-box">
-                <div class="card-title"
-                >Enterprise
-                </div>
-                <div class="card-subtitle">Enterprise Full-day</div>
-                <div class="card-contents">
-                    마이크로서비스의 전 과정을 심도있게 학습할 수 있는 과정으로 분석/설계, 구현, 배포, 운영 및 캡스톤 프로젝트를 포함하는 인텐시브 Course
-                </div>
-                <div style="padding-left:5px;  height:60px; line-height:40px; display:flex;">
-                    <div style="font-size:12px; font-weight:900;">247.5만원 ~ 420만원</div>
-                    <div style="margin:3px 0 0 3px; font-size:12px;">/1인 14일</div>
                 </div>
                 <div class="card-second-contents">
                     • 14 days<br>
@@ -463,6 +178,7 @@
         border: 1px solid #e5e5e5;
         border-radius: 5px;
         box-shadow: 0 1px 8px rgba(22,22,22, 0.1);
+        float:left;
     }
     .card-title {
         line-height:50px;
@@ -504,50 +220,64 @@
         line-height:36px !important;
         font-size:20px;
     }
+    .enterprise-cost-text {
+        font-size:14px;
+        margin-top:3px;
+        font-weight:900;
+    }
     .middle-box {
         display:none !important;
     }
     .mobile-box {
         display: none !important;
     }
-    @media only screen and (max-width:1280px){
-        .middle-box {
-            display:block !important;
-        }
-        .basics-box {
-            display:none !important;
-        }
+
+    @media only screen and (max-width:1700px){
         .card-box {
-            width:33%;
+            width:32.5%;
             height:550px;
-            margin-right:0.3%;
+            margin-right:0.5%;
             background-color: white;
             border: 1px solid #e5e5e5;
             border-radius: 5px;
             box-shadow: 0 1px 8px rgba(22,22,22, 0.1);
-            margin-top:20px;
+            float:left;
+        }
+        .enterprise-cost-text {
+            font-size:16px;
+            margin-top:3px;
+            font-weight:900;
+        }
+    }
+    
+    @media only screen and (max-width:1280px){
+        .card-box {
+            width:49.5%;
+            height:550px;
+            margin-right:0.5%;
+            background-color: white;
+            border: 1px solid #e5e5e5;
+            border-radius: 5px;
+            box-shadow: 0 1px 8px rgba(22,22,22, 0.1);
+            float:left;
+        }
+        .enterprise-cost-text {
+            font-size:18px;
+            margin-top:3px;
+            font-weight:900;
         }
     }
 
     @media only screen and (max-width:720px){
-        .middle-box {
-            display:none !important;
-        }
-        .basics-box {
-            display:none !important;
-        }
-        .mobile-box {
-            display: block !important;
-        }
         .card-box {
-            width:49%;
+            width:100%;
             height:550px;
             margin-right:0.5%;
-            margin-top:10px;
             background-color: white;
             border: 1px solid #e5e5e5;
             border-radius: 5px;
             box-shadow: 0 1px 8px rgba(22,22,22, 0.1);
+            float:left;
         }
     }
 </style>
