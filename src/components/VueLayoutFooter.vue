3055ecb70fd230734677e2e3031f18e03eea7310
Ju <tjdus0735@naver.com>
2024-05-31 18:00:26 +0900

    푸터배너추가

--- src/components/VueLayoutFooter.vue
+++ src/components/VueLayoutFooter.vue
@@ -1,13 +1,25 @@
 <template>
     <!--    <div class="py-2 border-t-2 border-ui-primary" style="background-color: #1e88e5;">-->
     <div class="py-10 border-t-2 border-ui-primary">
+        <a href="https://labs.msaez.io" target="_blank" class="footer-banner">
+            <g-image style="width:150px; height:150px; border-radius:15px; position:absolute; top:0; left:0;"
+                src="https://github.com/msa-ez/msaschool.github.io/assets/149130268/0dda67eb-8b79-4442-bdd9-c4526edaf6a8">
+            </g-image>
+            <g-image style="width:150px; height:150px; border-radius:15px; position:absolute; top:0; right:0;"
+                src="https://github.com/msa-ez/msaschool.github.io/assets/149130268/f8dac983-8824-402b-8a6b-e7ce0c1e1758">
+            </g-image>
+            <g-image style="height:150px; position:absolute; top:50%; left:50%; transform:translate(-50%, -50%);"
+                src="https://github.com/msa-ez/msaschool.github.io/assets/149130268/07fa7e92-2efc-4a9d-a857-d576b942030d">
+            </g-image>
+        </a>
+
         <div class="is-not-mobile-main-top" style="text-align:center;">
             <!-- <g-image style="width:150px; height:30px; position: absolute; left:50%;"
                 src="https://github.com/kykim97/factory-pattern/assets/113568664/2c714be7-81cc-4179-aa83-342e2f35c9d1">
             </g-image> -->
             <!-- <a href="https://labs.msaez.io" target="_blank" style="display: block; width: 30%; margin: 20px auto; margin-top: -10px;">
                 <g-image style="width:100%;"
-                    src="https://github.com/msa-ez/msaschool.github.io/assets/149130268/cd8ce703-c954-472f-9231-b16c717ae0c8">
+                    src="https://github.com/msa-ez/msaschool.github.io/assets/149130268/02ae596d-db77-4dff-bd05-92a70ceaf410">
                 </g-image>
             </a> -->
 
@@ -18,12 +30,6 @@
             <!-- <g-image style="width:100%;"
                 src="https://github.com/kykim97/factory-pattern/assets/113568664/2c714be7-81cc-4179-aa83-342e2f35c9d1"> 
             </g-image>-->
-            <!-- <a href="https://labs.msaez.io" target="_blank" class="footer-banner-m">
-                <g-image style="width:100%;"
-                    src="https://github.com/msa-ez/msaschool.github.io/assets/149130268/cd8ce703-c954-472f-9231-b16c717ae0c8">
-                </g-image>
-            </a> -->
-            
             
             <div>Copyright © uEngine All Rights Reserved</div>
             <div>주소 : 서울특별시 서초구 신반포로45길 18 502호(잠원동, 주일빌딩)</div>
@@ -121,11 +127,21 @@
             @apply text-ui-primary font-bold border-ui-primary;
         }
     }
-    .footer-banner-m {
-        display: block; 
-        width: 90%; 
-        margin: 15px auto; 
-        margin-top: 0;
+    // .footer-banner-m {
+    //     display: block; 
+    //     width: 90%; 
+    //     margin: 15px auto; 
+    //     margin-top: 0;
+    // }
+    .footer-banner {
+        display: block;
+        width: 95%;
+        height: 150px;
+        border-radius: 15px;
+        background-color: #9775c0;
+        margin: 0 auto;
+        margin-bottom: 30px;
+        position: relative;
     }
 
     @media only screen and (max-width:1410px){
@@ -136,28 +152,28 @@
             display:block;
         }
 
-        .footer-banner-m {
-            width: 40%; 
-        }
+        // .footer-banner-m {
+        //     width: 40%; 
+        // }
     }
     @media only screen and (max-width:1100px){
-        .footer-banner-m {
-            width: 50%; 
-        }
+        // .footer-banner-m {
+        //     width: 50%; 
+        // }
     }
     @media only screen and (max-width:960px){
-        .footer-banner-m {
-            width: 60%; 
-        }
+        // .footer-banner-m {
+        //     width: 60%; 
+        // }
     }
     @media only screen and (max-width:676px){
-        .footer-banner-m {
-            width: 70%; 
-        }
+        // .footer-banner-m {
+        //     width: 70%; 
+        // }
     }
     @media only screen and (max-width:460px){
-        .footer-banner-m {
-            width: 90%; 
-        }
+        // .footer-banner-m {
+        //     width: 90%; 
+        // }
     }
 </style>
