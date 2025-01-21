994701a840891c608376a385f1c9ac746c28a9d3
Ju <tjdus0735@naver.com>
2025-01-21 15:35:23 +0900

    차트반응형작업_및_툴팁수치항상보이게

--- src/components/mainPage/chartBox.vue
+++ src/components/mainPage/chartBox.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div>
+        <div style="background-color: #fff; padding: 20px; border-radius: 20px;">
             <!-- 차트 렌더링 공간 -->
             <canvas ref="barChart"></canvas>
         </div>
@@ -14,18 +14,31 @@ export default {
     data() {
         return {
             chartData: {
-                labels: ['January', 'February', 'March'],
+                labels: ["2019", "2020", "2021", "2022", "2023", "2024"],
                 datasets: [
                     {
-                        label: '일별',
-                        backgroundColor: '#8cc4ff',
-                        data: [40, 20, 12],
+                        label: "총 교육횟수",
+                        data: [52,58,64,73,28,50],
+                        backgroundColor: "#a1a2c6",
+                    },
+                    {
+                        label: '총 교육수강생',
+                        data: [1560,1740,1920,2190,980,1265],
+                        backgroundColor: '#96c596',
+                    },
+                    {
+                        label: '총 교육시간',
+                        data: [2030,2360,2560,2890,3720,4304],
+                        backgroundColor: '#8bc3da',
                     },
                 ],
             },
             chartOptions: {
                 responsive: true,
                 maintainAspectRatio: false,
+                legend: {
+                    display: false
+                },
                 scales: {
                     xAxes: [
                         {
@@ -35,6 +48,26 @@ export default {
                         },
                     ],
                 },
+                tooltips: {
+                    enabled: false
+                },
+                animation: {
+                    duration: 1,
+                    onComplete: function () {
+                        var chartInstance = this.chart,
+                            ctx = chartInstance.ctx;
+                        ctx.fillStyle = '#999';
+                        ctx.textAlign = 'left';
+                        ctx.textBaseline = 'top';
+                        this.data.datasets.forEach(function (dataset, i) {
+                            var meta = chartInstance.controller.getDatasetMeta(i);
+                            meta.data.forEach(function (bar, index) {
+                                var data = dataset.data[index];							
+                                ctx.fillText(data, bar._model.x + 5, bar._model.y - 5);
+                            });
+                        });
+                    }
+                }
             },
         };
     },
--- src/components/mainPage/mainChartAndCompanies.vue
+++ src/components/mainPage/mainChartAndCompanies.vue
@@ -4,11 +4,11 @@
             <h3 class="con-tit w400">MSA School 교육 실적 및 주요 수강 기업</h3>
         </div>
         <div class="chart-logo-box">
-            <div class="chart-box">
+            <div class="chart-wrapper">
                 <div class="chart-label">
                     <div>
                         <p style="font-weight: bold;">총 교육횟수</p>
-                        <p class="label-count">334회</p>
+                        <p class="label-count">325회</p>
                     </div>
                     <div>
                         <p style="font-weight: bold;">총 교육수강생</p>
@@ -19,12 +19,12 @@
                         <p class="label-count" style="background-color: #8bc3da;">17,864시간</p>
                     </div>
                 </div>
-                <div style="background-color: #fff; padding: 20px; border-radius: 20px;">
+                <div>
                     <chartBox />
                 </div>
             </div>
-            <div class="logo-box">
-                <g-image style="width: 100%;"
+            <div class="logo-wrapper">
+                <g-image class="logo-img"
                     src="https://www.uengine.org/images/case/major-companies-23add.jpg">
                 </g-image>
             </div>
@@ -57,23 +57,22 @@ export default {
     margin: 10px auto;
     text-align: center; 
 }
-
-/* 차트 좌측 라벨 */
-.chart-box {
+.chart-wrapper {
     display: flex;
     justify-content: space-around;
-    background-color: #f2f2f2;
+    background-color: #f6f6f6;
     padding: 20px;
+    width: 55%;
 }
-.chart-label {
+.chart-label { /* 차트 좌측 라벨 전체 박스 */
     display: block;
     align-content: center;
     text-align: center;
-    width: 130px;
+    width: 120px;
     margin-right: 10px;
 }
 .label-count {
-    width: 130px;
+    width: 120px;
     height: 34px;
     line-height: 34px;
     border-radius: 17px;
@@ -81,13 +80,65 @@ export default {
     color: #fff;
     margin-top: -10px;
 }
+.chart-bar {
+    width: 80%;
+    background-color: #fff; 
+    padding: 20px; 
+    border-radius: 20px;
+}
+.logo-wrapper {
+    width: 45%;
+    height: 440px;
+    line-height: 440px;
+    background-color: #f6f6f6;
+    position: relative;
+}
+.logo-img {
+    width: 100%; 
+    position: absolute; 
+    top: 50%; 
+    transform: translateY(-50%);
+}
 
-@media only screen and (max-width:676px) {
+@media only screen and (max-width: 1239px) {
+    .chart-wrapper {
+        width: 60%;
+    }
+    .logo-wrapper {
+        width: 40%;
+    }
+}
+
+@media only screen and (max-width: 1139px) {
+    .chart-wrapper {
+        width: 65%;
+    }
+    .logo-wrapper {
+        width: 35%;
+    }
+}
+
+@media only screen and (max-width: 1023px) {
     .chart-logo-box {
         display: block;
     }
-    /* 차트 */
-    .chart-box {
+    .chart-wrapper {
+        width: 100%;
+        justify-content: center;
+    }
+    .logo-wrapper {
+        width: 100%;
+        height: auto;
+    }
+    .logo-img {
+        position: relative;
+        top: 0;
+        transform: none;
+    }
+}
+
+@media only screen and (max-width: 676px) {
+    .chart-wrapper {
         display: block;
     }
     .chart-label {
@@ -98,6 +149,32 @@ export default {
     }
     .label-count {
         width: 110px;
+    }
+}
+
+@media only screen and (max-width: 480px) {
+    /* 차트 */
+    .chart-wrapper {
+        font-size: 15px;
+    }
+    .label-count {
+        width: 90px;
+        height: 28px;
+        line-height: 28px;
+        border-radius: 14px;
+    }
+}
+
+@media only screen and (max-width: 360px) {
+    /* 차트 */
+    .chart-wrapper {
+        font-size: 12px;
+    }
+    .label-count {
+        width: 70px;
+        height: 22px;
+        line-height: 22px;
+        border-radius: 11px;
     }
 }
 </style>
